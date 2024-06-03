8be203beca36a97865db1e9626b8486282279e0b
Joshna <vakatijoshna27@gmail.com>
2024-06-03 08:15:45 +0530

    Adding Lab001 file

---
+++ Ex_03062024/Lab001.py
@@ -0,0 +1,11 @@
+print('Hello world')
+print('Hello world')   #ctrl+D  gives duplicates
+print('Hello world')
+print('Hello world')
+print('Hello world')
+print('Hello world')
+print(2+2)
+print(2-2)
+print(2*2)
+print(2/2)
+
