627918f615dc3035274a8ad77edbb6023138cfd7
Joshna <vakatijoshna27@gmail.com>
2024-06-05 08:30:50 +0530

    Adding Lab001 file

---
+++ Ex_05042024/Lab002.py
@@ -0,0 +1,22 @@
+print()
+# args
+print('hello', 'world', 2, True, 3.14)
+
+# sep
+print('hello', 'world', 2, True, 3.14, sep=" ")
+print('hello', 'world', 2, True, 3.14, sep="-")
+print('hello', 'world', 2, True, 3.14, sep="")
+print('hi, my name is', 'Joshna', sep='  -  ')
+
+# end
+# print('hi, joshna vakati', end=" ")
+# print('hi, joshna vakati', end="\t")
+# print('hi, joshna vakati', end= '_')
+
+print('hi', 'joshna', sep='_')
+print('hi', 'joshna', end='_')
+print('hi joshna', end='_')
+
+# *args
+print(max(10, 23, 2))
+print(max(10, 23, 4, 56))
