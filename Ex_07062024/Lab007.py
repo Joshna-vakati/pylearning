05cc0cc64ade3f79730a5dbc2ee643dc9de94689
vakati <vakatijoshna27@gmail.com>
2024-06-07 07:43:40 +0530

    string

--- Ex_07062024/Lab007.py
+++ Ex_07062024/Lab007.py
@@ -1,1 +1,10 @@
 # python
+num1 = input('enter first number')
+num2 = input('enter second number')
+result = num1 + num2
+print(result)              # concatenate
+
+num1 = int(input('enter first number'))
+num2 = int(input('enter second number'))      #int
+result = num1 + num2
+print(result)
