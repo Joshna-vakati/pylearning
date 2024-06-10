f6df959556129a65cfb9010ce4bd4cd11e8b0b6e
vakati <vijaybabu@gmail.com>
2024-06-10 22:55:10 +0530

    area of a circle program

---
+++ Ex_10062024/Lab016.py
@@ -0,0 +1,10 @@
+#calculating the area of program
+
+import math
+
+radius = float(input('Enter the radius of the circle'))
+
+area = math.pi * (math.pow(radius,2))
+area2 = math.pi *(radius**2)
+print(area)
+print(area2)
