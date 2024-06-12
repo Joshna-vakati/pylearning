f5b6708192e8445d0ea8b625a4e5fdce27682257
vakati <vijaybabu@gmail.com>
2024-06-12 23:50:29 +0530

    Task 2 added

---
+++ Ex_10062024/Task_002.py
@@ -0,0 +1,21 @@
+#Develop a Python script that calculates
+# the square and cube of a given number. num = 2 sq - 4, c = 8
+
+num = 2
+square = num**2
+cube = num**3
+
+print(square)
+print(cube)
+
+
+# Create a program that takes two numbers as input and prints
+# whether the first number is greater than, less than, or equal to the second number.
+
+
+num1 = int(input('Enter the first number'))
+num2 = int(input('Enter the second number'))
+
+print(num1>num2)
+print(num1<num2)
+print(num1> num2)
