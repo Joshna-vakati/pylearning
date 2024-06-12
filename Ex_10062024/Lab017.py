3f2a6200f7263c0c3fb55cd672d4acb6ef7da81e
vakati <vijaybabu@gmail.com>
2024-06-12 23:09:05 +0530

    comparision/relationsl operators

---
+++ Ex_10062024/Lab017.py
@@ -0,0 +1,19 @@
+# comparision / relational operators
+
+#returns boolean value
+
+#<= , >= , == , !=, < ,>
+
+a = 10
+b = 5
+
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a<=b)
+print(a==b)
+print(a!=b)
+
+print(1==True)
+print(1 == False)
+
