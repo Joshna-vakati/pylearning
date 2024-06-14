866070b317062e1b9bd0eed0ac82e126765e2cb6
vakati <vijaybabu@gmail.com>
2024-06-14 09:04:36 +0530

    continue statement

---
+++ Ex_14062024/Lab029.py
@@ -0,0 +1,21 @@
+#pass statement
+
+#pass is a null statement in python.
+#it instructs to do nothing.
+#it is used when we don't want to write any code in a block.
+#for example:
+
+# syntax:
+# function/ condition / loop:
+#    pass
+
+s='joshna'
+for i in range(10):
+    if i==5:
+        pass
+    print(i)
+print('########################################')
+for i in s:
+    if i == 's':
+        pass
+    print(i)
