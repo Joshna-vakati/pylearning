35439d351e65b6f657ed591f8b7f8aa15f86527d
vakati <vijaybabu@gmail.com>
2024-06-22 12:50:53 +0530

    lambda function

---
+++ Ex_19062024/Lab037.py
@@ -0,0 +1,16 @@
+# lambda
+#lambda functions are anonymous functions that  are defined without a name
+
+# syntax :
+# lambda argument: expression
+
+# add 2 numbers
+
+add_num = lambda a,b : a+b
+print(add_num(4,5))
+
+cube = lambda x: x**3
+print(cube(3))
+
+
+
