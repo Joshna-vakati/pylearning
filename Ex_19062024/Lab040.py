45b1b8803df0ab0350c8945629dd3b4288c5480a
vakati <vijaybabu@gmail.com>
2024-06-22 22:12:55 +0530

    filter , zip

--- Ex_19062024/Lab040.py
+++ Ex_19062024/Lab040.py
@@ -12,3 +12,14 @@ lst = [1,2,3,4,5,6,7,8,9,10]
 result = filter(is_even, lst)
 res = filter(lambda x:x%2==0, lst)
 print(list(res))
+
+
+#zip
+
+# zip returns an iterator of tuples based on the iterable objects.
+last1 = [1,2,3,4,5]
+last2 = [6,7,8,9,10]
+res =[]
+for item1, item2 in zip(last1, last2):
+    res.append(item1+item2)
+print(res)
