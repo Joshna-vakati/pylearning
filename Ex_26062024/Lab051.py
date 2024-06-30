65c477b01e1036656439ccd2b71c927418e354ce
vakati <vijaybabu@gmail.com>
2024-06-30 23:57:04 +0530

    class attributes

--- Ex_26062024/Lab051.py
+++ Ex_26062024/Lab051.py
@@ -3,7 +3,7 @@
 # declared inside the class
 # shared and accessed by all class elements and outside the class.
 # they are called by class names
-# memory of class attributes shared by all the instamces of a class
+# memory of class attributes shared by all the instamces of a class.
 
 class employee:
     #class attributes
