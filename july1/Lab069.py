b9577fb15ccc19701bcbd48efb89b374dbcaedcb
vakati <vijaybabu@gmail.com>
2024-07-02 13:09:55 +0530

    multilevel inheritance

---
+++ july1/Lab069.py
@@ -0,0 +1,35 @@
+
+# Multilevel Inheritance
+class Grandparent:
+    def grandparent_method(self):
+        return "Grandparent's method"
+
+
+class Parent(Grandparent):
+    def parent_method(self):
+        return "Parent's method"
+
+
+class Child(Parent):
+    mac_m3_apple = "M3 Max"
+    def child_method(self):
+        return "Child's method"
+
+
+
+child = Child()
+print(child.grandparent_method())
+print(child.parent_method())
+print(child.child_method())
+print(child.mac_m3_apple())    # parent cannot access the child atttribute
+
+parent = Parent()
+parent.parent_method()
+parent.grandparent_method()
+parent.key_gold()             #parent can access base class
+
+
+
+grandparent_ref = Grandparent()
+grandparent_ref.grandparent_method()
+grandparent_ref.key_gold
