612de34f5701606de98537f8b056c2efc560fd5c
vakati <vijaybabu@gmail.com>
2024-07-02 13:13:17 +0530

    multiple inheritance

---
+++ july1/Lab070.py
@@ -0,0 +1,32 @@
+
+# Multiple Inheritance
+
+class Father:
+    def father_money(self):
+        return "5"
+
+    def home(self):
+        return "This is from the Father"
+
+
+class Mother:
+    def mother_money(self):
+        return "2"
+
+    def home(self):
+        return "This is from the Mother"
+
+
+class Son(Father, Mother ):
+    pass
+
+
+
+# Probelm - Diamond Problem - Java
+# Python - Multiple Inheritance
+# F,M -> S
+
+son = Son()
+son.father_money()
+son.mother_money()
+print(son.home())      # Method Resolution    ( mother and father both having home methods, what ever class u given ist , that has called)
