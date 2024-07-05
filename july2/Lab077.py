e92c9f45ca9285907bdb35ab23066ef40d1e4450
vakati <vijaybabu@gmail.com>
2024-07-05 23:53:45 +0530

    try statements with else

---
+++ july2/Lab077.py
@@ -0,0 +1,10 @@
+ # try statement with else
+def AbyB(a , b):
+     try:
+         c = ((a+b) / (a-b))
+     except ZeroDivisionError:
+         print ("a/b result in 0")
+     else:
+         print (c)
+AbyB(2.0, 3.0)
+AbyB(3.0, 3.0)
