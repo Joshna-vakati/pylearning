9b6eda58e388170ab552086be8fedf58cc732114
vakati <vijaybabu@gmail.com>
2024-07-06 20:57:32 +0530

    file handling (file open)

---
+++ july2/Lab081.py
@@ -0,0 +1,16 @@
+# file handling
+# python supports file handling and allows user to perform various
+# operations on file data
+
+# open a file
+# file = open(filename, mode)    # syntax
+# filename: name of the file to be opened
+# mode: mode in which the file should be opened (e.g. 'r' for read, 'w' for write, 'a' for append)
+
+# example: open a file in read mode
+f = open("demo.txt", "r")
+
+
+
+
+
