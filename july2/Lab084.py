7ea53d43100c140d6e80f952f0271df5c4a3608d
vakati <vijaybabu@gmail.com>
2024-07-06 21:44:02 +0530

    readlines

--- july2/Lab084.py
+++ july2/Lab084.py
@@ -7,3 +7,11 @@ with open('demo.txt', 'r') as file:
 
 # readline used to read line by line
 # readlines  used to read all lines and stores in a list
+
+
+# read words from file and print in reverse order
+with open('demo.txt', 'r') as file:
+    data = file.read().split()
+    # print(data)
+    for word in data[::-1]:
+        print(word)
