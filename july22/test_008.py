c5384852132cfe97d4cd3620a6062f628e08fe4c
vakati <vijaybabu@gmail.com>
2024-08-03 23:52:17 +0530

    Data driven using fixtures

--- july22/test_008.py
+++ july22/test_008.py
@@ -1,11 +1,12 @@
+
+
 import pytest
 @pytest.fixture()
 def loaddata():
     print("user profile data is being created")
     return ['Joshna', 'Nellore']
 
-
-@pytest.fixture(params = [("chrome", "Joshna", "Nellore), "firefox", "IE"])
+@pytest.fixture(params = [("chrome", "Joshna", "Nellore"), "firefox", "IE"])
 def crossBrowser(request): #even u use @pytest.mark.usefixtures("loaddata") , u need to pass fixture name as a parameter , if it has return statement
     return request.param
 
