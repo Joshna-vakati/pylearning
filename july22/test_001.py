99ca84cb2398a0eb3aea01ff1e2fbc3ea801c8c2
vakati <vijaybabu@gmail.com>
2024-08-03 22:28:58 +0530

    pytest basics

---
+++ july22/test_001.py
@@ -0,0 +1,22 @@
+# any pytest file should start with test_
+# any pytest method should start with the test 
+# any code should be thewrapped in method
+# each method is testcase
+# give test_testcasename as a method name
+
+
+def test_firstProgram():
+    print('Hello')
+
+
+def test_secondProgram():
+    print('How are you')
+
+
+
+
+
+
+
+
+
