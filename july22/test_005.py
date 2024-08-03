893009a8b27dad21d33c9ef6735047ccb3abf971
vakati <vijaybabu@gmail.com>
2024-08-03 23:16:03 +0530

    fixtures

--- july22/test_005.py
+++ july22/test_005.py
@@ -4,6 +4,8 @@ import pytest
 @pytest.fixture
 def setup():
     print("i will execute first")
+    yield
+    print('i will execute last ')
     
 def test_fixtureDemo(setup):
     print("i will execute aftr the fixture method")
