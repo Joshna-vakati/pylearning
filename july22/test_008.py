3dbfe03f02f0c4d1ceeab5126b88b21bed7ee5a8
vakati <vijaybabu@gmail.com>
2024-08-03 23:47:52 +0530

    parameterization(import multiple datasets)

--- july22/test_008.py
+++ july22/test_008.py
@@ -4,6 +4,7 @@ def loaddata():
     print("user profile data is being created")
     return ['Joshna', 'Nellore']
 
+
 @pytest.fixture(params = [("chrome", "Joshna", "Nellore), "firefox", "IE"])
 def crossBrowser(request): #even u use @pytest.mark.usefixtures("loaddata") , u need to pass fixture name as a parameter , if it has return statement
     return request.param
